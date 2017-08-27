d44ee4e48ccc9da0cc76e32364edbfe07b3196b0
kfindley7 <kfindleygt@gmail.com>
2017-08-26 22:20:58 -0400

    Made page dynamic for visualization page

---
+++ SerialToBrowser/public/home.js
@@ -0,0 +1,17 @@
+var listOfGames = ['ESCAPE FROM MARS', 'MINING FOR RESOURCES', 'ROCKET JUMP',
+    'SCAVENGER HUNT', 'SIMON SAYS', 'SPACEFOOD SQUEEZE', 'SPACE INVADERS',
+    'SPACE TRIVIA', 'WHACK-A-MOLE', 'GALAGA', 'PACMAN'];
+
+function showGames() {
+    var container = document.getElementById('listGames');
+    var start = 2;
+    var img = 1;
+    for (var i = 0; i < listOfGames.length; i++) {
+        container.innerHTML += "<div class=\"element js-game1\"><p style=\"padding-left: 0.5%\" class=\"text text-"+ start +"\">" + listOfGames[i] + "</p>\n" +
+            "      <p class=\"text text-"+ (start + 1) +"\"><span>Edit Game</span></p>\n" +
+            "      <img class=\"image image-1\" src=\"android-arrow-dropright.png\">\n" +
+            "    </div>";
+        start += 2;
+        img++;
+    }
+}
