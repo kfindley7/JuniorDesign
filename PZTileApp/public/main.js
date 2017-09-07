fe6c37509ea1a92026433fa3067b0277b1bc6801
whornsby <hornsbywilliamd@gmail.com>
2017-09-07 14:31:11 -0400

    Added pages for registering as a user and the code handling the database transactions. Edited homepage to link to registration page.

--- PZTileApp/public/main.js
+++ PZTileApp/public/main.js
@@ -7,6 +7,7 @@ $(function() {
     var $usernameInput = $('.js-username');
     var $passwordInput = $('.js-password');
     var $logoutButton = $('.js-logout-button');
+    var $registerButton = $('[id=register-button]');
 
     var username;
     var password;
@@ -39,6 +40,10 @@ $(function() {
         if (userConfirm) {
             window.location = "index.html";
         }
+    });
+
+    $registerButton.click(function(){
+       window.location = 'Register.html'
     });
 
     socket.on('login success', function () {
