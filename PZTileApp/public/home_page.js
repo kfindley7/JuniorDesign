ec2ecae1296f1a2ea76e986ad6acd67535c80fbe
kfindley7 <kfindleygt@gmail.com>
2017-11-09 13:29:12 -0500

    Issue #25: Fixed bug where sometimes when a game was clicked, it would be UNDEFINED

--- PZTileApp/public/home_page.js
+++ PZTileApp/public/home_page.js
@@ -20,19 +20,24 @@ $(function() {
     // add games to home-page.html after querying the DB
     function showGames(aList) {
         for (var i = 0; i < aList.length; i++) {
-            $listGames.append("<div class=\"element js-game1\">" +
-                "      <img class=\"image\" src=\"android-arrow-dropright.png\">\n" +
-                "      <p class=\"text-1\">Edit Game</p>\n" +
-                "<div class='game-text'><p class=\"text text-2\">" + aList[i].activityName + "</p></div>" +
-                "<div class='game-text'><p class=\"text text-3\">" + aList[i].activity + "</p></div>\n" +
+            $listGames.append("<div class=\"element js-game1\" onclick='$(window).getGameFromGameDiv(event)'>" +
+                "      <img class=\"image\" src=\"android-arrow-dropright.png\" onclick='$(window).getGameSelectedFromChildHtml(event)'>\n" +
+                "      <p class=\"text-1\" onclick='$(window).getGameSelectedFromChildHtml(event)'>Edit Game</p>\n" +
+                "<div class='game-text' onclick='$(window).getGameSelectedFromChildHtml(event)'><p class=\"text text-2\">" + aList[i].activityName + "</p></div>" +
+                "<div class='game-text' onclick='$(window).getGameSelectedFromChildHtml(event)'><p class=\"text text-3\">" + aList[i].activity + "</p></div>\n" +
                 "    </div>");
         }
     }
 
-    $listGames.click(function(item) {
-        console.log(item.target.childNodes[0].innerHTML);
-        window.location = "game-page.html?para=" + item.target.childNodes[0].innerHTML;
-    });
+    $.fn.getGameFromGameDiv = function (event) {
+        window.location.replace("game-page.html?para=" + event.target.childNodes[5].innerText);
+    };
+
+    $.fn.getGameSelectedFromChildHtml = function (event) {
+        window.location.replace("game-page.html?para=" + event.target.offsetParent.childNodes[5].innerText);
+        event.stopPropagation();
+    };
+
 
     $('#visual-button').click(function () {
         var userConfirm = confirm("Are you sure you want to leave this page? Any unsaved changes will be lost.");
